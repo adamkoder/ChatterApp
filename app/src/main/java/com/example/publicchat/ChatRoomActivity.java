479fc7f82706ac3980a869cf79e27d4d41753e80
AdamMaran <neophyte345@gmail.com>
2018-12-12 19:59:11 +0100

    different layouts for users, celar chat button

--- app/src/main/java/com/example/publicchat/ChatRoomActivity.java
+++ app/src/main/java/com/example/publicchat/ChatRoomActivity.java
@@ -1,8 +1,11 @@
 package com.example.publicchat;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +16,7 @@ import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.support.v7.widget.Toolbar;
@@ -107,7 +111,7 @@ public class ChatRoomActivity extends AppCompatActivity {
                 return true;
 
             case R.id.action_search:
-                showSearchPopupWindow(this.listView);
+                showSearchPopupWindow();
                 return true;
 
             default:
@@ -115,27 +119,23 @@ public class ChatRoomActivity extends AppCompatActivity {
         }
     }
 
-    public void showSearchPopupWindow(View view){
-        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-        View popupView = inflater.inflate(R.layout.search_popup_window, null);
+    public void showSearchPopupWindow(){
+        LayoutInflater inflater = (LayoutInflater) ChatRoomActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = inflater.inflate(R.layout.search_popup_window, null);
 
-        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
-        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
-        boolean focusable = true;
-        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+//        float density = ChatRoomActivity.this.getResources().getDisplayMetrics().density;
+        final PopupWindow popupWindow = new PopupWindow(layout, ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT, true);
 
-        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
-
-        popupView.setOnTouchListener(new View.OnTouchListener(){
-            @Override
-            public boolean onTouch(View v, MotionEvent event){
-                popupWindow.dismiss();
-                return true;
-            }
-        });
+//        ((ImageButton) layout.findViewById(R.id.popup_search_button).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view){
+//                popupWindow.dismiss();
+//            }
+//        });
+        popupWindow.showAtLocation(layout, Gravity.TOP,0, 0);
     }
 
-    //Finds the given keyword in the chatHistory list
+//    Finds the given keyword in the chatHistory list
 //    public void searchChatHistory(String){
 //        for(int i = 0; i < chatHistory.size(); i++){
 //            if(chatHistory.get(i).getText().toLowerCase().contains(""))
